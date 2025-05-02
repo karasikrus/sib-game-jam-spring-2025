a07230f553db063b431645bb703c872a429947a5
Daniil Smoliakov <danonofficial@gmail.com>
2025-05-02 21:36:52 +0100

    Interactable character

--- scenes/autoload/state_manager.gd
+++ scenes/autoload/state_manager.gd
@@ -3,6 +3,10 @@ extends Node
 enum GlobalStates {RUNNING, FREEZED}
 @onready var currentState = GlobalStates.RUNNING
 
+@onready var isMouseHighlightingObject = false
+
+@onready var isMovingToInteract = false
+
 func freeze_updates():
 	currentState = GlobalStates.FREEZED
 	

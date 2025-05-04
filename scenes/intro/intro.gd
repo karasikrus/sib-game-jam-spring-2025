221f1e5113e911c06edf48b157eaef2f0c3035ed
Daniil Smoliakov <danonofficial@gmail.com>
2025-05-04 17:01:41 +0100

    Intro

--- scenes/intro/intro.gd
+++ scenes/intro/intro.gd
@@ -3,12 +3,26 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
+	sprite.texture = intro_pictures[0]
 	pass # Replace with function body.
 
+@export var intro_pictures : Array[Texture]= []
+
+var current_picture = 0
+
+var timer = 0.0
+@export var show_for = 2.0
+@onready var sprite = $Sprite2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	LevelManager.load_specific_scene("res://scenes/zone1/zone1.tscn")
-	SceneTransition.open_screen()
+	timer += delta
+	if timer >= show_for:
+		current_picture += 1
+		timer = 0.0
+	if(current_picture == intro_pictures.size()):
+		LevelManager.load_specific_scene("res://scenes/zone1/zone1.tscn")
+		SceneTransition.open_screen()
+	else:
+		sprite.texture = intro_pictures[current_picture]
 	pass
